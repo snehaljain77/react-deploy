f67d9cd3cf28d63b2bb2de0af0bc1756739cf3c2
Snehal Jain <snehaljain115@gmail.com>
2021-03-20 21:50:06 +0530

    added animation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ function App() {
   return (
     <div className="App">
       <h1>Helu</h1>
+      <div class="card">
+		<div class="imgBox">
+			<div class="bark"></div>
+			<img src="https://image.ibb.co/fYzTrb/lastofus.jpg"></img>
+		</div>
+		<div class="details">
+			<h4 class="color2 margin">You Are Dino-Mite!</h4>
+			<p>Dear Dad,</p>
+			<p>Let's see.. .</p>
+			<p>You’re never around, you</p>
+			<p>hate the music I’m into, you</p>
+			<p>practically despise the movies I</p>
+			<p>like, and yet somehow you still</p>
+			<p>manage to be the best dad every year.</p>
+			<p>How do you do that? :)</p>
+			<p class="text-right">Happy Birthday, Sonakshi!</p>
+			<p class="text-right">♥Snehal</p>
+      <h4 class="color1">You Make My Heart Saur</h4>
+		</div>
+	</div>
     </div>
   );
 }
