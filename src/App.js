2613df93319139e3f9127f38cd70083c74e47c43
Snehal Jain <snehaljain115@gmail.com>
2021-03-25 21:17:25 +0530

    updated card

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Helu</h1>
+      <h1>Helu cutu</h1>
       <div class="card">
 		<div class="imgBox">
 			<div class="bark"></div>
 			<img src="https://image.ibb.co/fYzTrb/lastofus.jpg"></img>
 		</div>
 		<div class="details">
-			<h4 class="color2 margin">You Are Dino-Mite!</h4>
-			<p>Dear Dad,</p>
-			<p>Let's see.. .</p>
-			<p>You’re never around, you</p>
-			<p>hate the music I’m into, you</p>
-			<p>practically despise the movies I</p>
-			<p>like, and yet somehow you still</p>
-			<p>manage to be the best dad every year.</p>
-			<p>How do you do that? :)</p>
+			<h4 class="color2 margin">You Are Rawr-some!</h4>
+			<p>Dear Sonz,</p>
+			<p>I'm so happy that you're in my</p>
+			<p>life, you always make me smile, even</p>
+			<p>on my worst days, you are absolutely</p>
+			<p>adorable, I fucking love you with</p>
+			<p>all my heart and wish you all the</p>
+      <p>happiness in the world. Muah, have</p>
+			<p>fun, You're a dino-mite :))</p>
 			<p class="text-right">Happy Birthday, Sonakshi!</p>
 			<p class="text-right">♥Snehal</p>
-      <h4 class="color1">You Make My Heart Saur</h4>
+      <h4 class="color1">You Make My Heart Saur!</h4>
 		</div>
 	</div>
     </div>
